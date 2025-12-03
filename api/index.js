98f5adcdb54df253f912aa31d7e6b67ddfbbdc9a
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2025-12-03 18:53:02 +0530

    update

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "../routes/authRoutes.js";
-import { connectToDatabase } from "./db.js";
+import { connectToDatabase } from "../api/db.js";
 
 dotenv.config();
 
